9484077b1bf0ba21680cdddc90fbc14de8144c0a
Jose Antonio Sánchez <crimson3d@gmail.com>
2024-04-19 15:10:05 +0200

    new challenge

---
+++ challenges/interactive-rating-component-main/assets/js/app.js
@@ -0,0 +1,28 @@
+const score = document.querySelectorAll(".stars__rate");
+let finalSelectedScore;
+
+score.forEach(selectedScore => {
+    selectedScore.addEventListener("click", function() {
+        score.forEach(scoreElement => {
+            scoreElement.style.backgroundColor = "hsla(216, 12%, 54%, 0.25)";
+            scoreElement.style.color = "hsl(217, 12%, 63%)";
+        });
+        this.style.backgroundColor = "hsl(25, 97%, 53%)";
+        this.style.color = "white";
+        finalSelectedScore = selectedScore.innerHTML;
+    });
+});
+
+const boton = document.getElementById("rateButton");
+const spanDestino = document.getElementById("finalSelectedScore");
+const primeraTarjeta = document.getElementById("stateStart");
+const segundaTarjeta = document.getElementById("stateFinal");
+
+boton.addEventListener("click", function() {
+    if (finalSelectedScore !== undefined) {
+        spanDestino.innerHTML = finalSelectedScore;
+        primeraTarjeta.style.display = "none";
+        segundaTarjeta.style.display = "flex";
+    }
+});
+
