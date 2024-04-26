4bcc2edcadf6b6ac139c424738d5c9a8e1f9d57c
Jose Antonio Sánchez <crimson3d@gmail.com>
2024-04-26 17:52:45 +0200

    app.js update

--- challenges/newsletter-sign-up-with-success-message-main/assets/js/app.js
+++ challenges/newsletter-sign-up-with-success-message-main/assets/js/app.js
@@ -2,7 +2,8 @@ const signUpSection = document.getElementById("signUp");
 const successSection = document.getElementById("success");
 const emailInput = document.getElementById("email");
 const form = document.getElementById("form");
-const dismiss = document.getElementById("dismissButton")
+const dismiss = document.getElementById("dismissButton");
+const errorMessage = document.getElementById("error");
 
 let validateEmail = (email) => {
     const check = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
@@ -14,15 +15,17 @@ form.addEventListener('submit', function(event){
 
     if (validateEmail(emailInput.value)) {
         console.log('Valid Email!');
+        errorMessage.innerHTML = '';
         successSection.classList.remove('hidden');
         signUpSection.classList.add('hidden');
     } else {
         console.log('Invalid Email');
-        alert('Por favor introduce un email válido');
+        errorMessage.innerHTML = 'Valid Email Required';
     }
 });
 
 dismiss.addEventListener('click', function(){
     successSection.classList.add('hidden');
     signUpSection.classList.remove('hidden');
+    errorMessage.innerHTML = '';
 });
