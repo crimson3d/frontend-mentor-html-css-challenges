656e4e14df8128773cdbec535c6bd92425cfdc80
Jose Antonio Sánchez <crimson3d@gmail.com>
2024-05-06 12:01:39 +0200

    Update app.js

--- challenges/time-tracking-dashboard-main/assets/js/app.js
+++ challenges/time-tracking-dashboard-main/assets/js/app.js
@@ -1,22 +1,100 @@
-const dayButton = document.getElementById('daily');
-const weekButton = document.getElementById('weekly');
-const monthButton = document.getElementById('monthly');
-let work = document.getElementById('workTime');
-let lastWork = document.getElementById('lastWorkTime');
-let play = document.getElementById('playTime');
-let lastPlay = document.getElementById('lastPlayTime');
-let study = document.getElementById('studyTime');
-let lastStudy = document.getElementById('lastStudyTime');
-let exercise = document.getElementById('exerciseTime');
-let lastExercise = document.getElementById('lastExerciseTime');
-let social = document.getElementById('socialTime');
-let lastSocial = document.getElementById('lastSocialTime');
-let selfCare = document.getElementById('selfCareTime');
-let lastSelfCare = document.getElementById('lastSelfCareTime');
+let dataTime = [];
 
 
-fetch('/challenges/time-tracking-dashboard-main/data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    });
+// Función para recuperar datos del JSON
+async function fetchData() {
+    try {
+        const response = await fetch('/challenges/time-tracking-dashboard-main/data.json');
+        dataTime = await response.json();
+        showData('option1');
+        showData('option2');
+        showData('option3');
+        cleanDivs();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', fetchData);
+
+// Función para limpiar divs que muestran la info del JSON
+function cleanDivs() {
+    document.getElementById('workTime').textContent = '';
+    document.getElementById('lastWorkTime').textContent = '';
+    document.getElementById('playTime').textContent = '';
+    document.getElementById('lastPlayTime').textContent = '';
+    document.getElementById('studyTime').textContent = '';
+    document.getElementById('lastStudyTime').textContent = '';
+    document.getElementById('exerciseTime').textContent = '';
+    document.getElementById('lastExerciseTime').textContent = '';
+    document.getElementById('socialTime').textContent = '';
+    document.getElementById('lastSocialTime').textContent = '';
+    document.getElementById('selfCareTime').textContent = '';
+    document.getElementById('lastSelfCareTime').textContent = '';
+};
+
+
+
+
+// Función para actualizar los divs según el botón pulsado
+function showData(option) {
+   // Aquí buscamos los objetos de los que queremos recuperar los datos
+   let work = dataTime.find(actividad => actividad.title === 'Work');
+   let play = dataTime.find(actividad => actividad.title === 'Play');
+   let study = dataTime.find(actividad => actividad.title === 'Study');
+   let exercise = dataTime.find(actividad => actividad.title === 'Exercise');
+   let social = dataTime.find(actividad => actividad.title === 'Social');
+   let selfCare = dataTime.find(actividad => actividad.title === 'Self Care');
+
+    // Decidimos que datos mostrar según la opción elegida
+    switch (option) {
+        case 'option1':
+            document.getElementById('workTime').textContent = work.timeframes.daily.current;
+            document.getElementById('lastWorkTime').textContent = work.timeframes.daily.previous;
+            document.getElementById('playTime').textContent = play.timeframes.daily.current;
+            document.getElementById('lastPlayTime').textContent = play.timeframes.daily.previous;
+            document.getElementById('studyTime').textContent = study.timeframes.daily.current;
+            document.getElementById('lastStudyTime').textContent = study.timeframes.daily.previous;
+            document.getElementById('exerciseTime').textContent = exercise.timeframes.daily.current;
+            document.getElementById('lastExerciseTime').textContent = exercise.timeframes.daily.previous;
+            document.getElementById('socialTime').textContent = social.timeframes.daily.current;
+            document.getElementById('lastSocialTime').textContent = social.timeframes.daily.previous;
+            document.getElementById('selfCareTime').textContent = selfCare.timeframes.daily.current;
+            document.getElementById('lastSelfCareTime').textContent = selfCare.timeframes.daily.previous;
+            break;
+        case 'option2':
+            document.getElementById('workTime').textContent = work.timeframes.weekly.current;
+            document.getElementById('lastWorkTime').textContent = work.timeframes.weekly.previous;
+            document.getElementById('playTime').textContent = play.timeframes.weekly.current;
+            document.getElementById('lastPlayTime').textContent = play.timeframes.weekly.previous;
+            document.getElementById('studyTime').textContent = study.timeframes.weekly.current;
+            document.getElementById('lastStudyTime').textContent = study.timeframes.weekly.previous;
+            document.getElementById('exerciseTime').textContent = exercise.timeframes.weekly.current;
+            document.getElementById('lastExerciseTime').textContent = exercise.timeframes.weekly.previous;
+            document.getElementById('socialTime').textContent = social.timeframes.weekly.current;
+            document.getElementById('lastSocialTime').textContent = social.timeframes.weekly.previous;
+            document.getElementById('selfCareTime').textContent = selfCare.timeframes.weekly.current;
+            document.getElementById('lastSelfCareTime').textContent = selfCare.timeframes.weekly.previous;
+            break;
+        case 'option3':
+            document.getElementById('workTime').textContent = work.timeframes.monthly.current;
+            document.getElementById('lastWorkTime').textContent = work.timeframes.monthly.previous;
+            document.getElementById('playTime').textContent = play.timeframes.monthly.current;
+            document.getElementById('lastPlayTime').textContent = play.timeframes.monthly.previous;
+            document.getElementById('studyTime').textContent = study.timeframes.monthly.current;
+            document.getElementById('lastStudyTime').textContent = study.timeframes.monthly.previous;
+            document.getElementById('exerciseTime').textContent = exercise.timeframes.monthly.current;
+            document.getElementById('lastExerciseTime').textContent = exercise.timeframes.monthly.previous;
+            document.getElementById('socialTime').textContent = social.timeframes.monthly.current;
+            document.getElementById('lastSocialTime').textContent = social.timeframes.monthly.previous;
+            document.getElementById('selfCareTime').textContent = selfCare.timeframes.monthly.current;
+            document.getElementById('lastSelfCareTime').textContent = selfCare.timeframes.monthly.previous;
+            break;
+    }
+};
+
+document.addEventListener ('DOMContentLoaded', () => {
+    document.getElementById('daily').onclick = () => showData('option1');
+    document.getElementById('weekly').onclick = () => showData('option2');
+    document.getElementById('monthly').onclick = () => showData('option3');
+});
